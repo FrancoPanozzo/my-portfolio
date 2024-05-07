8bc835b27bee21621eb8e8fa141e00e26d370fa6
Franco <francopanozzo@hotmail.com.ar>
2024-05-07 19:16:06 +0200

    Tailwind init

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+
+export default <Partial<Config>>{
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue',
+  ],
+
+  darkMode: ['class'],
+
+  theme: {
+    fontFamily: {
+      title: ['Roboto', 'sans-serif'],
+      body: ['Lato', 'sans-serif']
+    },
+
+    extend: {
+      colors: {
+        primary: {
+          ...colors.red,
+          DEFAULT: colors.red['600']
+        }
+      },
+    }
+  },
+}
