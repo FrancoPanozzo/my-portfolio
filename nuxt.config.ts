dfbd3c7eb59b8af359bc4fa818421991c9cf3558
Franco <francopanozzo@hotmail.com.ar>
2024-05-07 20:43:39 +0200

    Add i18n, nuxt image, icon and seo modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode']
-})
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/i18n",
+    "nuxt-icon",
+    "@nuxt/image",
+    "@nuxtjs/seo",
+  ],
+  colorMode: {
+    preference: "system", // default value of $colorMode.preference
+    fallback: "dark", // fallback value if not system preference found
+    classSuffix: "",
+    storageKey: "nuxt-color-mode",
+  },
+  i18n: {
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "lang_locale",
+      redirectOn: "root", // recommended
+    },
+  },
+});
